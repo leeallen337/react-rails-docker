9cbe82e0d22bc72ad45376121ccc4b8027d43046
Lee Allen <leeallen337@gmail.com>
2017-04-24 20:49:08 -0400

    Updating with front-end code

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {};
+
+function App() {
+  return (
+    <div>
+      Hello World
+    </div>
+  );
+}
+
+App.propTypes = propTypes;
+
+export default App;
---
+++ frontend/webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const config = {
+  context: __dirname + '/src',
+  entry: {
+    bundle: './index.jsx'
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html'
+    })
+  ],
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json']
+  },
+  devServer: {
+    historyApiFallback: true
+  }
+};
+
+module.exports = config;
